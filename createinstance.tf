99712da68ef3a15e88a549a8eef9cbb55c3c3c1f
saddam <saddam312514@gmail.com>
2023-10-19 15:25:54 +0600

    change

--- createinstance.tf
+++ createinstance.tf
@@ -20,4 +20,19 @@ resource "aws_instance" "myfirstinstace" {
 
 }
 
+resource "aws_instance" "private-instance" {
+    ami = "ami-08e5424edfe926b43"
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.levelup_key.key_name
+    vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
+    subnet_id = aws_subnet.levelupvpc-private-1.id
+    associate_public_ip_address = false
+    
+    
+    tags = {
+      Name = "private-instance"
+    }
+
+}
+
 
